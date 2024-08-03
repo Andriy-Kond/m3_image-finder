bcb78500b34dc2497045d85adcb9b373ca47eb89
Andrii <andrii.kondratovich@gmail.com>
2024-08-03 16:47:53 +0300

    add: components templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <></>;
+// import { MdOutlineImageSearch } from "react-icons/md";
+// <MdOutlineImageSearch />
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
---
+++ src/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ImageGallery extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default ImageGallery;
